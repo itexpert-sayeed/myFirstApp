649248a208297832f07624e8616fdf159f1f9580
Sayeed Rahman <itexpert.sayeed@gmail.com>
2022-12-01 21:51:29 +0600

    1st Commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput 
+        placeholder='Place Anything'
+        style={styles.input}
+        
+        />
+        <Button 
+        title='Add'/>
+      </View>
+     
+      
     </View>
   );
 }
@@ -15,6 +24,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputView : {
+    width: '100%',
+        padding: 20,
+         flexDirection:'row',
+         marginTop: 50,
+         justifyContent: 'space-between',
+         alignItems: 'center', 
+  },
+  input :{
+    width: '80%',
+    borderBottomWidth: 3,
+    borderColor: 'blue',
+    padding: 5,
+  }
 });
